863959c6112c12a5c9adc5e4f2aefd1cb1f917fc
DM <vitatwo@gmail.com>
2013-04-27 01:17:05 +0100

    Included localStorage to save values

--- script.js
+++ script.js
@@ -1,4 +1,36 @@
 jQuery(document).ready(function($) {
+	//global variables/functions
+	var currentKeyCount = window.localStorage.length;
+	var secondsVar = new Date().getTime() / 1000;
+
+
+
+	function identifyKey(thisValue){
+
+	for (var key in localStorage){
+		var itemValue = window.localStorage.getItem(key);
+
+		if(itemValue === thisValue){
+			return key;
+		}
+	}
+
+
+
+
+
+/*		var x = 0;
+		while(x<=currentKeyCount){
+			var value = window.localStorage.getItem(x);
+			if(value === thisValue){
+				return window.localStorage.key(x);
+			}else{
+				x++;
+			}
+		}*/
+	}
+
+
 	$('#addToList').submit(function(){
 
 		var userInput = $('#userInput').val();
@@ -7,20 +39,30 @@ jQuery(document).ready(function($) {
 		if(userInput === ""){
 			return false;
 		}else{
-
+			//change the key ID
+			//key = Index ID, value = userInput
+			window.localStorage.setItem(secondsVar, userInput);
 			$('<li><span class="listText">' +userInput+ '</span> <button id="strikeOff">&#10003;</button> <button id="delete">&#10007;</button> <button id="edit">EDIT</button> </li>').appendTo('#list');
 
-			return false;
+			//return false;
 		};
 	});
 
-	//scrach off completed items
+	//scrach off completed items / Toggle CSS class
 	$("#strikeOff").live('click', function(){
 		$(this).parent('li').toggleClass('done');
 	});
 
 	//delete items
 	$("#delete").live('click', function(){
+
+	var thisValue = $(this).parent('li').children('span').text();
+	var thisKey = identifyKey(thisValue);
+
+		window.localStorage.removeItem(thisKey);
+		//alert(thisKey);
+
+
 		$(this).parent('li').remove();
 	});
 
@@ -37,6 +79,8 @@ jQuery(document).ready(function($) {
 				var newItem = $('#editBox').val();
 
 				$(this).parent('li').children('span').html(newItem);
+
+
 
 				//remove edit form
 				$('#editForm').remove();
